d9dfebc7b1c66a676e05bacfb711c33ea45192a1
Gokul S <gokuls2781@gmail.com>
2020-11-11 13:09:01 +0530

    added

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,12 +1,19 @@
 const User = require("../models/User")
+
 const login = (req, res, next) => {
-    
+    const {email, password} = req.body
+    User.findById({email}).then(user => {
+        if(!user) res.send({message : "User not found"})
+        res.status(200).send(user)
+    }).catch((e) => res.status(400).send(e))
 }
-const signUp = async (req, res) => {
+
+const signUp =  (req, res) => {
     const {name, email, password} = req.body
     const user = new User({name, email, password})
-    await user.save()
-    res.send({message : "Success"})
+    user.save().then(user => {
+        res.status(200).send({message : "Success"})
+    }).catch((e) => res.status(400).send(e))
 }
 
 module.exports = { login, signUp}
